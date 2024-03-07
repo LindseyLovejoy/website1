4a7528717a51c526e50789d1cd862eeb74408ae1
LindseyLovejoy <143131172+LindseyLovejoy@users.noreply.github.com>
2024-03-07 21:50:47 +0000

    Thu, Mar 7, 2024, 1:50 PM -08:00

--- js/navagation.js
+++ js/navagation.js
@@ -1,7 +1,8 @@
-nav = document.quereSelector('.nav')
+nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
+    console.log(window.scrollY)
     if (window.scrollY > 223) {
         nav.classList.add('active');
     } else
