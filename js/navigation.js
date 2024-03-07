b09831aed846b38d8fc238bd2d77c6ecd0e74299
LindseyLovejoy <143131172+LindseyLovejoy@users.noreply.github.com>
2024-03-07 21:53:34 +0000

    Thu, Mar 7, 2024, 1:53 PM -08:00

--- js/navigation.js
+++ js/navigation.js
@@ -1,11 +1,10 @@
 nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
+window.addEventListener('scroll', fixNav
 
 function fixNav() {
     console.log(window.scrollY)
     if (window.scrollY > 223) {
         nav.classList.add('active');
-    } else
-    {
+    } else {
         nav.classList.remove('active');
     }
